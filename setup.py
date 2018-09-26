bc5a478392996e8417d30c1022fc7600ace29cdd
Kashyap <neelabh.kashyap@cgi.com>
2018-09-26 13:46:20 +0300

    Tweaked metadata

--- setup.py
+++ setup.py
@@ -6,14 +6,21 @@ except ImportError:
 config = {
     'description': 'Module to access azure blob CSV files with Pandas',
     'author': 'Neelabh Kashyap',
-    'url': '',
-    'download_url': '',
+    'url': 'https://github.com/nihil0/bluepandas',
     'author_email': 'neelabh.kashyap@cgi.com',
     'version': '0.1',
-    'install_requires': ['pytest'],
+    'install_requires': ['azure-storage-blob', 'pandas'],
     'packages': find_packages(exclude=['contrib', 'docs', 'tests']),
     'scripts': [],
-    'name': 'bluepandas'
+    'name': 'bluepandas',
+    'classifiers': [
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
 }
 
 setup(**config)
