5569a8bab6c00f92402dc6829056bc45b47a8355
Neelabh Kashyap <neelabh2@gmail.com>
2018-09-28 15:05:44 +0300

    Now able to write to blob storage as well (#1)
    * implemented safer URL parsing
    
    * WIP: bluepandas DataFrame
    
    * actual commit for pull request :P

--- bluepandas/bluepandas.py
+++ bluepandas/bluepandas.py
@@ -3,18 +3,43 @@ Module to access Azure Blob Storage objects in CSV format with Pandas
 """
 from __future__ import print_function
 
-import sys
-import os
-
-if sys.version_info.major == 3:
-    from io import StringIO
-else:
-    from StringIO import StringIO
-
 from azure.storage.blob import BlockBlobService 
 
+import sys
+import os
 import re
 import pandas as pd
+
+from io import BytesIO, StringIO
+
+class DataFrame(pd.DataFrame):
+    def plain_to_csv(self, path_or_buf=None, **kwargs):
+        return super(DataFrame, self).to_csv(path_or_buf=path_or_buf, **kwargs)
+
+    def to_csv(self, path_or_buf=None, **kwargs):
+        try:
+            container, storage_account_name, blob_name = parse_url(path_or_buf)    
+        except (URLParsingError, TypeError):
+            return self.plain_to_csv(path_or_buf=path_or_buf, **kwargs)
+        else:
+            conn_string = get_connection_string(storage_account_name)
+            blob = BlockBlobService(connection_string=conn_string)
+
+            def progressbar(x, y):
+                x = 0 if x is None else x
+                y = 0 if y is None else y
+
+                print("{0:0.2f} kB of {1:0.2f} kB downloaded.".format(x / 1024.0, y / 1024.0), end="\r")
+
+            text = self.plain_to_csv(**kwargs)
+            
+            blob.create_blob_from_text(container,
+                                            blob_name,
+                                            text,
+                                            progress_callback=progressbar
+                                        )
+
+
 
 class ConnectionStringError(Exception):
     """
@@ -22,14 +47,38 @@ class ConnectionStringError(Exception):
     """
     pass
 
+class URLParsingError(Exception):
+    """
+    Raised if bluepandas is unable to parse the WASB(S) URL
+    """
+    pass
+
 def parse_url(az_url):
     """
     Returns the names of the container, the storage account and the blob from
-    a WASB(S) url. 
-    """
-    url_regex = re.compile("wasbs?://([a-z0-9\\-]*(?=@))@([a-z0-9]*)\\.blob\\.core\\.windows\\.net/(.*)")
+    a WASB(S) url.
 
-    return re.match(url_regex, az_url).groups()
+    Args:
+        az_url (str): WASBS URL
+
+    Returns
+        tuple: (container, storage account name, blob name)
+
+    Raises:
+        :class:`~bluepandas.bluepandas.URLParsingError`: When the tuple above
+            cannot be extracted from the URL.
+
+    """
+    url_regex = re.compile("wasbs?://([a-z0-9\\-]*(?=@))@([a-z0-9]*)\\.blob\\.core\\.windows\\.net/([^\\s]+)")
+
+    match = re.match(url_regex, az_url)
+
+    if match is None:
+        raise URLParsingError("Please check the URL provided.")
+    else:
+        return match.groups()
+
+
 
 def get_connection_string(storage_account_name):
     """
@@ -37,13 +86,15 @@ def get_connection_string(storage_account_name):
     returns the corresponding connection string. Raises a 
     ``ConnectionStringNotFound`` exception if environment variable is missing
     """
-    
-    conn_string = os.environ.get("AZ_"+storage_account_name.upper(), None)
+
+    conn_string = os.environ.get("AZ_" + storage_account_name.upper(), None)
 
     if conn_string is None:
-        raise ConnectionStringError("Environment variable AZ_"+storage_account_name.upper()+" not found!")
+        raise ConnectionStringError(
+            "Environment variable AZ_" + storage_account_name.upper() + " not found!")
     else:
         return conn_string
+
 
 def read_csv(az_url, **kwargs):
     """
@@ -55,15 +106,19 @@ def read_csv(az_url, **kwargs):
     blob = BlockBlobService(connection_string=conn_string)
 
     def progressbar(x, y):
-        print("{0:0.2f} kB of {1:0.2f} kB downloaded.".format(x/1024.0,y/1024.0), end="\r")
+        print("{0:0.2f} kB of {1:0.2f} kB downloaded.".format(x / 1024.0, y / 1024.0), end="\r")
 
-    csvstr = blob.get_blob_to_text( 
-                        container_name,
-                        blob_name,
-                        progress_callback=progressbar
-                        ).content
+    csvstr = blob.get_blob_to_text(container_name,
+                                   blob_name,
+                                   progress_callback=progressbar
+                                   ).content
 
-    return pd.read_csv(StringIO(csvstr), **kwargs)
+    df = pd.read_csv(StringIO(csvstr), **kwargs)
+    
+    return DataFrame(df.values, columns = df.columns)
+
+
+
 
     
 
--- test/test_bluepandas.py
+++ test/test_bluepandas.py
@@ -6,7 +6,6 @@ from __future__ import absolute_import
 import pandas as pd
 
 from os import path, environ
-import sys
 import pytest
 
 import bluepandas
@@ -20,25 +19,32 @@ my_urls = [
     "wasbs://test-container@bluepandas.blob.core.windows.net/banklist.csv",
     "wasbs://test-container@bluepandas.blob.core.windows.net/dir/long/CrAzyT3xt%&¤&.csv",    
     "wasbs://test-container@greenpandas.blob.core.windows.net/banklist.csv",
-    "wasbs://test-container@bluepandas.blob.core.windows.net/Monthly-train.csv"
-]
+    "wasbs://test-container@bluepandas.blob.core.windows.net/Monthly-train.csv",
+    "wasb://test-container@bluepandas.blob.core.windows.net/ ",
+    "wasbs://test-container@bluepandas.blob.core.windows.net/",
+    "wasbs://test-container@bluepandas.blob.core.windows.net/banklist_written.csv"
+    ]
 
 account_conn_string = "DefaultEndpointsProtocol=https;AccountName=bluepandas;AccountKey=xxxx;EndpointSuffix=core.windows.net"
 sas_conn_string = "BlobEndpoint=https://bluepandas.blob.core.windows.net/;QueueEndpoint=https://bluepandas.queue.core.windows.net/;FileEndpoint=https://bluepandas.file.core.windows.net/;TableEndpoint=https://bluepandas.table.core.windows.net/;SharedAccessSignature=xxxx"
 
+
 def test_parse_urls():
     assert bluepandas.parse_url(my_urls[0]) == ("test-container", "bluepandas",
-            "banklist.csv")
+                                                "banklist.csv")
 
     assert bluepandas.parse_url(my_urls[1]) == ("test-container", "bluepandas",
-            "banklist.csv")
+                                                "banklist.csv")
 
     assert bluepandas.parse_url(my_urls[2]) == ("test-container", "bluepandas",
-            "dir/long/CrAzyT3xt%&¤&.csv")
+                                                "dir/long/CrAzyT3xt%&¤&.csv")
+    with pytest.raises(bluepandas.URLParsingError):
+        bluepandas.parse_url(my_urls[5])
+        bluepandas.parse_url(my_urls[6])
     
 def test_get_connection_string():
     with pytest.raises(bluepandas.ConnectionStringError):
-        environ.pop("AZ_GREENPANDAS", None) # In case environment contains this 
+        environ.pop("AZ_GREENPANDAS", None)  # In case environment contains this 
         bluepandas.get_connection_string("greenpandas")
     
     environ["AZ_BLUEPANDAS"] = account_conn_string
@@ -51,12 +57,26 @@ def test_get_connection_string():
 
     assert ("AccountKey" in keys1) or ("SharedAccessSignature" in keys2) 
 
+
 def test_read_csv():
     environ["AZ_BLUEPANDAS"] = secret_conn_string
 
     df_reference = pd.read_csv(path.join(test_assets_path, "banklist.csv"))
+    df_reference = bluepandas.DataFrame(df_reference.values,
+                                        columns=df_reference.columns)
+
     df_test = bluepandas.read_csv(my_urls[1])
 
     assert df_reference.equals(df_test)
+
+
+def test_write_csv():
+    df = bluepandas.DataFrame([(1,2), (3,4)], columns = ['A', 'B'])
+    assert df.to_csv() == ",A,B\n0,1,2\n1,3,4\n"
+    assert df.to_csv(index=False) == "A,B\n1,2\n3,4\n"
+    
+    df.to_csv(my_urls[7], index=False)
+    df_test = bluepandas.read_csv(my_urls[7])
+    assert df.equals(df_test)
     
     
